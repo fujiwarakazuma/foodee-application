b5d4a7d78e5ce13460b4fb3d9631f6e1214bf3f8
fujiwarakazuma <fujiwara.kazuma.2002@gmail.com>
2024-08-01 20:01:04 +0900

    php artisan migrate

--- database/migrations/2024_07_22_092446_create_posts_table.php
+++ database/migrations/2024_07_22_092446_create_posts_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("user_id")-constrained();
-            $table->foreignId("region_id")-constrained();
-            $table->foreignId("category_id")-constrained();
+            $table->foreignId("user_id")->constrained();
+            $table->foreignId("region_id")->constrained();
+            $table->foreignId("category_id")->constrained();
             $table->string("store");
             $table->string("body");
             $table->timestamps();
--- database/migrations/2024_07_22_092711_create_saves_table.php
+++ database/migrations/2024_07_22_092711_create_saves_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('saves', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("user_id")-constrained();
-            $table->foreignId("post_id")-constrained();
+            $table->foreignId("user_id")->constrained();
+            $table->foreignId("post_id")->constrained();
             $table->timestamps();
         });
     }
